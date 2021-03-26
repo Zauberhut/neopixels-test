4e7708802f448b96f2e08255fa2eda176b1e4996
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2021-03-26 08:34:50 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,12 @@
-let range = 60
-let strip = neopixel.create(DigitalPin.P1, range, NeoPixelMode.RGB_RGB)
-strip.setBrightness(255)
-let Position = range / 2
-strip.showRainbow(1, 360)
-strip.show()
+let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB_RGB)
+strip.setBrightness(150)
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
 basic.forever(function () {
-    if (Position <= 0) {
-        Position = 0
-    }
-    if (Position >= range) {
-        Position = range - 1
-    }
-})
-basic.forever(function () {
-    strip.setPixelColor(Position, neopixel.colors(NeoPixelColors.Red))
     if (input.isGesture(Gesture.TiltLeft)) {
-        Position += 1
+        strip.rotate(1)
     }
     if (input.isGesture(Gesture.TiltRight)) {
-        Position += -1
+        strip.rotate(-1)
     }
     strip.show()
-    basic.pause(100)
 })
