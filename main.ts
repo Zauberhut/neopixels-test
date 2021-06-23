9f45262eb6ff1fb8f16e69d58488cfcfd8c4a564
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2021-06-23 22:42:17 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB_RGB)
+let strip = neopixel.create(DigitalPin.P0, 75, NeoPixelMode.RGB_RGB)
 strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
 basic.forever(function () {
     strip.rotate(Math.map(input.acceleration(Dimension.X), 0, 1023, 0, 4))
